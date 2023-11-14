4ebabcaeab5eb91a1dae653edd3736082ed2364c
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-11-14 10:34:14 +0530

    node js work

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
+require('dotenv').config();
 const express = require('express');
-
 const server = express();
 
 server.use(express.static('public'));
@@ -8,9 +8,9 @@ server.use(express.static('public'));
 server.get('/homepage',(request,response)=>{
     // response.send('<h1>Hello</h1>')
     response.json({name:"sarfraj abbasi"})
-})
+}) 
 
-server.listen(8080,()=>{
-    console.log("server started");
+server.listen(process.env.PORT,()=>{
+    console.log("server started at",process.env.PORT);
 })
 
