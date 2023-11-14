8f11f0e4fd3775fed22a208b12e353f7dcbe85f7
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-11-14 10:21:08 +0530

    node js work

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const server = express();
 
 server.use(express.static('public'));
 
+// GET localhost:8080/homepage
+server.get('/homepage',(request,response)=>{
+    // response.send('<h1>Hello</h1>')
+    response.json({name:"sarfraj abbasi"})
+})
+
 server.listen(8080,()=>{
     console.log("server started");
 })
