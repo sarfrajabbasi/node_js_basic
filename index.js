0813536f46b46e92df9567c6ec36dc7ac6186088
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-11-19 19:38:06 +0530

    node

--- index.js
+++ index.js
@@ -169,6 +169,8 @@ function isAuthenticated(req,res,done){
 // Register:- 
 server.post('/register',(req,res,done)=>{
 
+const salt = bcrypt.genSaltSync(saltRounds);
+
 User.findOne({username:username},(err,user)=>{
   if(err)done(null,false);
   else if(user)res.redirect('/');
