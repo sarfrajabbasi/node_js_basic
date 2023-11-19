0bf52e028bf7e732748f2937e778b2d600e0d940
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-11-19 19:37:14 +0530

    node

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const session = require("express-session")
 // bcrypt setup
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const myPlaintextPassword = 's0/\/\P4$$w0rD';
 
 // mongoose
 const mongoose = require('mongoose');
